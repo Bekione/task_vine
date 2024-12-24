3967362d2151fa48763b90e9ad80d42ba09eb781
bekione <kinber024@gmail.com>
2024-12-24 15:12:04 +0300

    Enhance Todo type definitions and state management
    - Added a constant for the default todo status.
    - Made the description field in the Todo interface optional.
    - Introduced State type to manage todos and draggedTodo.
    - Defined Actions type for todo-related operations: add, drag, remove, update, and reorder todos.

--- types/todo.ts
+++ types/todo.ts
@@ -1,10 +1,25 @@
 export type TodoStatus = 'todo' | 'in-progress' | 'done'
 
+// Add a constant for the default status
+export const DEFAULT_TODO_STATUS: TodoStatus = 'todo'
+
 export interface Todo {
   id: string
   title: string
-  description: string
+  description?: string
   status: TodoStatus
   createdAt: Date
 }
 
+export type State = {
+  todos: Todo[],
+  draggedTodo: string | null
+}
+
+export type Actions = {
+  addTodo:  (title: string, description?: string) => void
+  dragTodo: (id: string | null) => void
+  removeTodo: (id: string) => void
+  updateTodo: (id: string, status: TodoStatus) => void
+  reorderTodo: (newTodos: Todo[]) => void
+}
