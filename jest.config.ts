d0787356527fa7d9e80748e7bcd3a61c5dcccfbf
bekione <kinber024@gmail.com>
2024-12-27 14:12:59 +0300

    feat(tests): add Jest configuration and setup files for testing environment
    - Introduced jest.config.ts to configure Jest with Next.js integration, specifying test environment and coverage collection.
    - Added jest.setup.ts to set up testing utilities, including mocks for window.matchMedia, localStorage, and next/navigation.
    - This change establishes a robust testing framework, enhancing the application's testability and reliability.

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(nanoid|@radix-ui|framer-motion|next-pwa|zustand)/)',
+  ],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  collectCoverageFrom: [
+    'components/**/*.{ts,tsx}',
+    'lib/**/*.{ts,tsx}',
+    'hooks/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+  ],
+}
+
+export default createJestConfig(config) 
