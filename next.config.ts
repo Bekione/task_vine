dfd1636de103425b3ed066208451fae81680c678
bekione <kinber024@gmail.com>
2024-12-27 14:12:16 +0300

    feat(pwa): update Next.js configuration for PWA support and offline fallback
    - Modified the Next.js configuration to integrate PWA support with enhanced options.
    - Added a fallback for offline access by specifying a document to display when offline.
    - This change improves the application's performance and user experience during offline scenarios.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 const withPWA = require('next-pwa');
 
-const config: NextConfig = withPWA({
+const config: NextConfig = {
+  // Your other Next.js config options here if needed
+};
+
+export default withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-});
-
-export default config;
+  disable: process.env.NODE_ENV === 'development',
+  fallbacks: {
+    document: '/offline'
+  }
+})(config);
